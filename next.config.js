bbabd17dfeafc04ecd5a3e3c0149f460af3304e1
Vit Bokisch <vit@bokisch.cz>
2021-02-24 16:02:52 +0100

    config cleanup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
-// const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-// const CONFIG = require('./config')
 
 const PLUGINS = [[optimizedImages, {}]]
 
@@ -21,20 +19,4 @@ const PLUGINS = [[optimizedImages, {}]]
 module.exports = withPlugins(PLUGINS, {
   poweredByHeader: false,
   reactStrictMode: true,
-  webpack: (config) => {
-    // config.plugins.push(
-    //   new FaviconsWebpackPlugin({
-    //     logo: CONFIG.favicon,
-    //     cache: true,
-    //     outputPath: CONFIG.publicDir,
-    //     prefix: CONFIG.publicDir,
-    //     icons: {
-    //       coast: false,
-    //       yandex: false,
-    //     },
-    //   })
-    // )
-
-    return config
-  },
 })

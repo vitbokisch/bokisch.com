a3a541c6bee4c1b346ed18bb5a944d20645448b4
Vit Bokisch <vit@bokisch.cz>
2021-03-04 11:48:20 +0100

    footer styling

--- src/components/base/skeleton/Footer/index.tsx
+++ src/components/base/skeleton/Footer/index.tsx
@@ -10,8 +10,9 @@ export default element
     contentAlignX: 'center',
   })
   .theme((t, m) => ({
-    height: 80,
+    height: { xs: 60, md: 80 },
     lineHeight: t.lineHeight.reset,
     backgroundColor: m(t.color.light[100], t.color.dark[100]),
     color: m(t.color.dark[100], t.color.light[100]),
+    fontSize: { xs: t.fontSize.small, md: t.fontSize.base },
   }))

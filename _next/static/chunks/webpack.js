c2ffdb78c3ad3f653205acef56af9b9f3f719b11
Vit Bokisch <vit@bokisch.cz>
2021-03-08 12:22:01 +0100

    Release new version

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9763b3c4e15cf9a79dc2";
+/******/ 	var hotCurrentHash = "df3f2cfc4ec1739e0373";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

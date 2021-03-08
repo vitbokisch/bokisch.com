ab7ab1b9ee1d91722c6b8ec081540b9cb2d47fb6
Vit Bokisch <vit@bokisch.cz>
2021-03-08 12:21:30 +0100

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
 const PLUGINS = [
+  [
+    withPWA,
+    { pwa: { dest: 'public', sw: 'service-worker.js', runtimeCaching } },
+  ],
   [
     optimizedImages,
     {

7cefde95881348f9797c026f84991725ced85453
Vit Bokisch <vit@bokisch.cz>
2021-04-15 13:24:51 +0200

    Release new version

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "554b97267e06a3df7aa7"; }
+/******/ 		__webpack_require__.h = function() { return "b3f677675ef70394954b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

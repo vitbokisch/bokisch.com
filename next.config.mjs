be41797ab6ddd0ed9f17c38ece56c2220eddeb74
Vit Bokisch <vit@bokisch.cz>
2023-01-29 12:13:03 +0100

    deps update

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,13 @@ import optimizedImages from 'next-optimized-images-2'
 
 const PLUGINS = [
   [
-    optimizedImages,
-    {
+    optimizedImages({
       optimizeInDev: true,
-      svgo: {
-        plugins: [{ name: 'removeViewBox', active: false }],
-      },
-    },
+    }),
   ],
 ]
 
 export default withPlugins(PLUGINS, {
-  dir: 'src',
   poweredByHeader: false,
   compiler: {
     // removeConsole: true,
